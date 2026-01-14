e5b3f79870bd62e69a87f62d4c0b00f3c2b44d5e
arimisarn <arimisa.dev@gmail.com>
2026-01-14 06:47:54 +0300

    Fix staticfiles config for Render

--- project/settings.py
+++ project/settings.py
@@ -12,7 +12,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # =====================
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-dev')
 DEBUG = os.environ.get('DEBUG') == 'True'
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "nathar.onrender.com",
+]
+
 
 # =====================
 # APPLICATIONS
