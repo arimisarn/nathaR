3f679f885bf9733ab6e8db1cfe6c551dc9d7f886
arimisarn <arimisa.dev@gmail.com>
2026-01-14 06:57:31 +0300

    Fix staticfiles config for Render

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from .views import login_view, register_view, welcome_view, logout_view
+def home(request):
+    return redirect('login')
 
 urlpatterns = [
+    path('', home, name='home'),  # ← IMPORTANT
+
     path('login/', login_view, name='login'),
     path('register/', register_view, name='register'),
     path('welcome/', welcome_view, name='welcome'),
